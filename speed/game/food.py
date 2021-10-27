ae5a1d005d9edd974b0c3c675f6674011a09765a
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-10-27 09:36:41 -0500

    update

---
+++ speed/game/food.py
@@ -0,0 +1,22 @@
+import random
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+# TODO: Define the Food class here
+class Food(Actor):
+    """The food class. It keeps track of everything that the food is doing. moves it when needed and when collected helps to grow the snakes tail."""
+
+
+    def __init__(self):
+        pass
+
+
+    def get_points(self):
+        """This helps to call _points whenever it is needed without changing the origional"""
+
+        return self._points
+
+    def reset(self):
+        """moves the food to a new random location then it's picked up."""
+        pass
