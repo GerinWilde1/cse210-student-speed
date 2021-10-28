70510f8b1387edffe8c86a046fb8e5a63bf45ef9
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-10-27 21:18:51 -0500

    Update Gerin_test.py

--- speed/game/Gerin_test.py
+++ speed/game/Gerin_test.py
@@ -2,6 +2,8 @@ import constants
 import os
 import random
 
+#from point import Point
+
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
 list = []
@@ -9,3 +11,10 @@ for i in range(constants.STARTING_WORDS):
     list.append(LIBRARY[random.randint(0, 10000)])
     #print(LIBRARY[random.randint(0, 10000)])
 print(list)
+
+
+x = random.randint(2, constants.MAX_X)
+y = random.randint(2, constants.MAX_Y)
+#postition = Point(x, y)
+#self.set_position(postiion)
+print(x, y)
