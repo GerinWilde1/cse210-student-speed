ee8b9fe97a34bfed624c4e2b51449a19f3ee500c
geraldqf <91680068+geraldqf@users.noreply.github.com>
2021-10-29 08:27:24 -0500

    Update word.py

--- speed/game/word.py
+++ speed/game/word.py
@@ -4,7 +4,7 @@ from game import constants
 from game.actor import Actor
 from game.point import Point
 
-# TODO: Define the Food class here
+# TODO: Define the Word class here
 class Word(Actor):
     """The food class. It keeps track of everything that the food is doing. moves it when needed and when collected helps to grow the snakes tail."""
     
